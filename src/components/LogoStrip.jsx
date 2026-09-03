92cd936fb444f05219734e3eeff44c9bba186fde
Pritam Paul <paulpritam417@gmail.com>
2026-09-03 22:05:26 +0530

    fixed file path of comsoc

--- src/components/LogoStrip.jsx
+++ src/components/LogoStrip.jsx
@@ -5,7 +5,7 @@ import ieee_sb from "../assets/image/Ieee_cs_kol_white.png";
 import iedc from "../assets/image/IEDC_White_Logo.png";
 import ieee_cs_sb from "../assets/image/pritam2.png";
 import cycrai_logo from "../assets/image/cycrai_latest_logo.png";
-import comsoc from "../assets/image/comsoc.png";
+import comsoc from "../assets/image/Comsoc.png";
 
 export default function LogoStrip() {
   const logos = [
